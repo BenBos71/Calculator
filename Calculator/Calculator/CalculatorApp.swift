de54ce3ec6c4429aef75c602da79b06280080583
BenBos <ben.bos1097@gmail.com>
2022-05-29 17:08:19 -0600

    Basic Calculator

---
+++ Calculator/Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Ben Bos on 4/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
